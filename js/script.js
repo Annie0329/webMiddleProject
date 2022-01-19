2d62a876e220860b4ee19b99e7113838d84e2a01
Annie0329 <0329annie@gmail.com>
2022-01-19 11:56:15 +0800

    0119

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ var wow = new WOW({
     offset: 200,
 });
 wow.init();
+$(document).ready(function () {
+    LottieInteractivity.create({
+        player: '#firstLottie',
+        mode: "cursor",
+        actions: [
+            {
+                type: 'toggle',
+            },
+        ]
+    });
+    $("lottie-player").click(function () {
+        player.play();
+    });
+});
